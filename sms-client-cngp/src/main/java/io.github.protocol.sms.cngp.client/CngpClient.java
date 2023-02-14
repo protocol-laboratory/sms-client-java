738fd0949e6627fce6bfeeae4fcef4b4b2b78911
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2023-02-14 15:47:15 +0800

    cngp and smgp client impl. (#3)

--- sms-client-cngp/src/main/java/io.github.protocol.sms.cngp.client/CngpClient.java
+++ sms-client-cngp/src/main/java/io.github.protocol.sms.cngp.client/CngpClient.java
@@ -19,9 +19,21 @@
 
 package io.github.protocol.sms.cngp.client;
 
+import io.github.protocol.codec.cngp.CngpConst;
 import io.github.protocol.codec.cngp.CngpDecoder;
 import io.github.protocol.codec.cngp.CngpEncoder;
+import io.github.protocol.codec.cngp.CngpExit;
+import io.github.protocol.codec.cngp.CngpExitResp;
+import io.github.protocol.codec.cngp.CngpHeader;
+import io.github.protocol.codec.cngp.CngpLogin;
+import io.github.protocol.codec.cngp.CngpLoginBody;
+import io.github.protocol.codec.cngp.CngpLoginResp;
+import io.github.protocol.codec.cngp.CngpLoginRespBody;
 import io.github.protocol.codec.cngp.CngpMessage;
+import io.github.protocol.codec.cngp.CngpSubmit;
+import io.github.protocol.codec.cngp.CngpSubmitBody;
+import io.github.protocol.codec.cngp.CngpSubmitResp;
+import io.github.protocol.codec.cngp.CngpSubmitRespBody;
 import io.github.protocol.sms.client.util.BoundAtomicInt;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
@@ -35,6 +47,10 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 public class CngpClient extends SimpleChannelInboundHandler<CngpMessage> {
 
@@ -46,9 +62,16 @@ public class CngpClient extends SimpleChannelInboundHandler<CngpMessage> {
 
     private ChannelHandlerContext ctx;
 
+    private volatile CompletableFuture<CngpLoginRespBody> loginFuture;
+
+    private final Map<Integer, CompletableFuture<CngpSubmitRespBody>> submitFutures;
+
+    private volatile CompletableFuture<Void> exitFuture;
+
     public CngpClient(CngpClientConfig config) {
         this.config = config;
         this.seq = new BoundAtomicInt(0x7FFFFFFF);
+        this.submitFutures = new ConcurrentHashMap<>();
     }
 
     public void start() throws Exception {
@@ -83,6 +106,45 @@ public class CngpClient extends SimpleChannelInboundHandler<CngpMessage> {
         }
     }
 
+    public CompletableFuture<CngpLoginRespBody> loginAsync(CngpLoginBody loginBody) {
+        CompletableFuture<CngpLoginRespBody> future = new CompletableFuture<>();
+        CngpHeader header = new CngpHeader(CngpConst.LOGIN_ID, 0, seq.nextVal());
+        ctx.writeAndFlush(new CngpLogin(header, loginBody)).addListener(f -> {
+            if (f.isSuccess()) {
+                loginFuture = future;
+            } else {
+                future.completeExceptionally(f.cause());
+            }
+        });
+        return future;
+    }
+
+    public CompletableFuture<CngpSubmitRespBody> submitAsync(CngpSubmitBody submitBody) {
+        CompletableFuture<CngpSubmitRespBody> future = new CompletableFuture<>();
+        CngpHeader header = new CngpHeader(CngpConst.SUBMIT_ID, 0, seq.nextVal());
+        ctx.writeAndFlush(new CngpSubmit(header, submitBody)).addListener(f -> {
+            if (f.isSuccess()) {
+                submitFutures.put(header.sequenceId(), future);
+            } else {
+                future.completeExceptionally(f.cause());
+            }
+        });
+        return future;
+    }
+
+    public CompletableFuture<Void> exitAsync() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        CngpHeader header = new CngpHeader(CngpConst.EXIT_ID, 0, seq.nextVal());
+        ctx.writeAndFlush(new CngpExit(header)).addListener(f -> {
+            if (f.isSuccess()) {
+                exitFuture = future;
+            } else {
+                future.completeExceptionally(f.cause());
+            }
+        });
+        return future;
+    }
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         this.ctx = ctx;
@@ -90,6 +152,43 @@ public class CngpClient extends SimpleChannelInboundHandler<CngpMessage> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, CngpMessage msg) throws Exception {
+        if (msg instanceof CngpLogin) {
+            processLogin((CngpLogin) msg);
+        } else if (msg instanceof CngpLoginResp) {
+            processLoginResp((CngpLoginResp) msg);
+        } else if (msg instanceof CngpSubmitResp) {
+            processSubmitResp((CngpSubmitResp) msg);
+        } else if (msg instanceof CngpExitResp) {
+            processExitResp((CngpExitResp) msg);
+        }
+    }
+
+    private void processLogin(CngpLogin cngpLogin) {
+        throw new IllegalStateException("client side can't process login");
+    }
+
+    private void processLoginResp(CngpLoginResp cngpLoginResp) {
+        if (loginFuture == null) {
+            throw new IllegalStateException("login future is null");
+        }
+        loginFuture.complete(cngpLoginResp.body());
+    }
+
+    private void processSubmitResp(CngpSubmitResp cngpSubmitResp) {
+        CompletableFuture<CngpSubmitRespBody> future = submitFutures.remove(cngpSubmitResp.header().sequenceId());
+        if (future == null) {
+            log.warn("submit future is null, sequence id is {}", cngpSubmitResp.header().sequenceId());
+            return;
+        }
+        future.complete(cngpSubmitResp.body());
+    }
+
+    private void processExitResp(CngpExitResp cngpExitResp) {
+        if (exitFuture == null) {
+            log.warn("exit future is null, sequence id is {}", cngpExitResp.header().sequenceId());
+            return;
+        }
+        exitFuture.complete(null);
     }
 
     public void stop() {
--- sms-client-smgp/src/main/java/io.github.protocol.sms.smgp.client/SmgpClient.java
+++ sms-client-smgp/src/main/java/io.github.protocol.sms.smgp.client/SmgpClient.java
@@ -19,9 +19,19 @@
 
 package io.github.protocol.sms.smgp.client;
 
+import io.github.protocol.codec.smgp.SmgpConst;
 import io.github.protocol.codec.smgp.SmgpDecoder;
 import io.github.protocol.codec.smgp.SmgpEncoder;
+import io.github.protocol.codec.smgp.SmgpHeader;
+import io.github.protocol.codec.smgp.SmgpLogin;
+import io.github.protocol.codec.smgp.SmgpLoginBody;
+import io.github.protocol.codec.smgp.SmgpLoginResp;
+import io.github.protocol.codec.smgp.SmgpLoginRespBody;
 import io.github.protocol.codec.smgp.SmgpMessage;
+import io.github.protocol.codec.smgp.SmgpSubmit;
+import io.github.protocol.codec.smgp.SmgpSubmitBody;
+import io.github.protocol.codec.smgp.SmgpSubmitResp;
+import io.github.protocol.codec.smgp.SmgpSubmitRespBody;
 import io.github.protocol.sms.client.util.BoundAtomicInt;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
@@ -35,6 +45,10 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 public class SmgpClient extends SimpleChannelInboundHandler<SmgpMessage> {
 
@@ -46,9 +60,14 @@ public class SmgpClient extends SimpleChannelInboundHandler<SmgpMessage> {
 
     private ChannelHandlerContext ctx;
 
+    private volatile CompletableFuture<SmgpLoginRespBody> loginFuture;
+
+    private final Map<Integer, CompletableFuture<SmgpSubmitRespBody>> submitFuture;
+
     public SmgpClient(SmgpClientConfig config) {
         this.config = config;
         this.seq = new BoundAtomicInt(0x7FFFFFFF);
+        this.submitFuture = new ConcurrentHashMap<>();
     }
 
     public void start() throws Exception {
@@ -83,6 +102,32 @@ public class SmgpClient extends SimpleChannelInboundHandler<SmgpMessage> {
         }
     }
 
+    public CompletableFuture<SmgpLoginRespBody> loginAsync(SmgpLoginBody smgpLoginBody) {
+        CompletableFuture<SmgpLoginRespBody> future = new CompletableFuture<>();
+        SmgpHeader header = new SmgpHeader(SmgpConst.LOGIN_ID, seq.nextVal());
+        ctx.writeAndFlush(new SmgpLogin(header, smgpLoginBody)).addListener(f -> {
+            if (f.isSuccess()) {
+                loginFuture = future;
+            } else {
+                future.completeExceptionally(f.cause());
+            }
+        });
+        return future;
+    }
+
+    public CompletableFuture<SmgpSubmitRespBody> submitAsync(SmgpSubmitBody smgpSubmitBody) {
+        CompletableFuture<SmgpSubmitRespBody> future = new CompletableFuture<>();
+        SmgpHeader header = new SmgpHeader(SmgpConst.SUBMIT_ID, seq.nextVal());
+        ctx.writeAndFlush(new SmgpSubmit(header, smgpSubmitBody)).addListener(f -> {
+            if (f.isSuccess()) {
+                submitFuture.put(header.sequenceID(), future);
+            } else {
+                future.completeExceptionally(f.cause());
+            }
+        });
+        return future;
+    }
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         this.ctx = ctx;
@@ -90,6 +135,33 @@ public class SmgpClient extends SimpleChannelInboundHandler<SmgpMessage> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, SmgpMessage msg) throws Exception {
+        if (msg instanceof SmgpLogin) {
+            processLogin((SmgpLogin) msg);
+        } else if (msg instanceof SmgpLoginResp) {
+            processLoginResp((SmgpLoginResp) msg);
+        } else if (msg instanceof SmgpSubmitResp) {
+            processSubmitResp((SmgpSubmitResp) msg);
+        }
+    }
+
+    private void processLogin(SmgpLogin smgpLogin) {
+        throw new IllegalStateException("client side can't process login");
+    }
+
+    private void processLoginResp(SmgpLoginResp smgpLoginResp) {
+        if (loginFuture == null) {
+            throw new IllegalStateException("login future is null");
+        }
+        loginFuture.complete(smgpLoginResp.body());
+    }
+
+    private void processSubmitResp(SmgpSubmitResp smgpSubmitResp) {
+        CompletableFuture<SmgpSubmitRespBody> future = submitFuture.remove(smgpSubmitResp.header().sequenceID());
+        if (future == null) {
+            log.warn("submit future is null, sequence id is {}", smgpSubmitResp.header().sequenceID());
+            return;
+        }
+        future.complete(smgpSubmitResp.body());
     }
 
     public void stop() {
